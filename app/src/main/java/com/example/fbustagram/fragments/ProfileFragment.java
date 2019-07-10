bf19b52898f14b76240b36129fba9fd62b138ce7
Ariana Adames <ariana.adames2@gmail.com>
2019-07-10 16:33:09 -0700

    First Commit

---
+++ app/src/main/java/com/example/fbustagram/fragments/ProfileFragment.java
@@ -0,0 +1,99 @@
+package com.example.fbustagram.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.fbustagram.MainActivity;
+import com.example.fbustagram.Post;
+import com.example.fbustagram.PostsAdapter;
+import com.example.fbustagram.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProfileFragment extends Fragment {
+
+    Button bLogOut;
+    RecyclerView rvPosts;
+    protected PostsAdapter adapter;
+    protected List<Post> mPosts;
+    public final String TAG = "ProfileFragment";
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        rvPosts = view.findViewById(R.id.rvPosts);
+        //create the adapter and data source
+        mPosts = new ArrayList<>();
+        adapter = new PostsAdapter(getContext(), mPosts);
+        // set the adapter on the recycler view
+        rvPosts.setAdapter(adapter);
+        // set the layout manager on the recycler view
+        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+        queryPosts();
+        bLogOut = (Button) view.findViewById(R.id.bLogOut);
+
+        bLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser();// this will now be null
+                Intent intent = new Intent(getContext(), MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+
+
+    protected void queryPosts() {
+        ParseQuery<Post> postQuery = new ParseQuery<Post>(Post.class);
+        postQuery.include(Post.KEY_USER);
+        postQuery.setLimit(20);
+        postQuery.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser());
+        postQuery.addDescendingOrder(Post.KEY_CREATED_AT);
+        postQuery.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+
+                if (e != null){
+                    Log.e(TAG, "Error with query");
+                    e.printStackTrace();
+                    return;
+                }
+                mPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+
+                for (int i = 0; i < posts.size(); i++){
+                    Post post = posts.get(i);
+                    Log.d(TAG, "Post: " + post.getDescription() +", user: " + post.getUser().getUsername());
+                }
+            }
+        });
+    }
+}
+
+
