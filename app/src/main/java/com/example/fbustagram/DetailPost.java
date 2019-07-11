a93b00b3cecf1d149994cc5c1056783276a097c6
Ariana Adames <ariana.adames2@gmail.com>
2019-07-10 23:29:03 -0700

    Fixed detailed post layout

--- app/src/main/java/com/example/fbustagram/DetailPost.java
+++ app/src/main/java/com/example/fbustagram/DetailPost.java
@@ -1,10 +1,13 @@
 package com.example.fbustagram;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.bumptech.glide.Glide;
 
@@ -16,21 +19,42 @@ public class DetailPost extends AppCompatActivity {
     private Post post;
     TextView tvDescription;
     TextView tvHandle;
+    TextView tvLikes;
+    TextView tvUserDescription;
     ImageView ivImage;
+    ImageButton ibLike;
+    ImageButton ibReply;
+    ImageButton ibComment;
+    ImageButton ibSave;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_post);
-        Post post = (Post) Parcels.unwrap(getIntent().getParcelableExtra("post"));
+        //ButterKnife.bind(DetailPost.this);
 
-        ivImage = findViewById(R.id.ivImage);
-        tvHandle = findViewById(R.id.ivHandle);
+        ConstraintLayout constraintLayout = findViewById(R.id.layout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
+
+        post = (Post) Parcels.unwrap(getIntent().getParcelableExtra("post"));
+
         tvDescription = findViewById(R.id.tvDescription);
+        tvHandle = findViewById(R.id.tvHandle);
+        tvLikes = findViewById(R.id.tvLikes);
+        tvUserDescription = findViewById(R.id.tvUserDescription);
+        ivImage = findViewById(R.id.ivImage);
+        ibLike = findViewById(R.id.ibLike);
+        ibReply = findViewById(R.id.ibReply);
+        ibComment = findViewById(R.id.ibComment);
+        ibSave = findViewById(R.id.ibSave);
 
         tvHandle.setText(post.getUser().getUsername());
         tvDescription.setText(post.getDescription());
+        tvUserDescription.setText(post.getUser().getUsername());
         Glide.with(this).load(post.getImage().getUrl()).into(ivImage);
 
 
